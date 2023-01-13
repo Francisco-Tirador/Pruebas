e603e77cf2e5e20058a693ba773258af12df1e3d
FraBeltran <jcfranciscoBeltran@gmail.com>
2023-01-13 17:44:24 -0600

    segundo aporte

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,56 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import banner from './assets/cpt1.png'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
   return (
     <div className="App">
       <div className='ContenRegister'>
         <div className='Conten_campos'>
-          <div className='Ingresar'>
-
+         <img src={banner} alt="" />
+          
           </div>
           <div className='Registro'>
+          <div className="fondo f1"></div>
+          <span className='fondo f2'></span>
             <form >
 
               <ul className="login_list">
                 <div className='input1'>
-                <li className="loginItem">
+                <li className="loginItem"><img className='icon' src="https://cdn-icons-png.flaticon.com/512/848/848043.png" alt="" />
                   <input type="text" className=" editedImput " placeholder={"PREFIJO"} /></li>
 
-                <li className="loginItem"><label htmlFor="login-pass" className="login__label"></label>
+                <li className="loginItem"><img className='icon' src="https://cdn-icons-png.flaticon.com/512/848/848043.png" alt="" />
                   <input type="text" className=" editedImput" id='login-pass' placeholder={"NOMBRE(S)"} /></li>
 
-                  <li className="loginItem">
+                  <li className="loginItem"><img className='icon' src="https://cdn-icons-png.flaticon.com/512/848/848043.png" alt="" />
                   <input type="text" className=" editedImput " placeholder='APELLIDOS' /></li>
 
-                  <li className="loginItem">
+                  <li className="loginItem"><img className='icon' src="https://cdn-icons-png.flaticon.com/512/4574/4574730.png" alt="" />
                   <input type="email" className=" editedImput " placeholder='EMEIl' /></li>
 
-                  <li className="loginItem">
-                  <input type="tel" className=" editedImput " placeholder='TELEFONO DE CONTACTO' /></li>
+                  <li className="loginItem"><img className='icon' src="https://cdn-icons-png.flaticon.com/512/126/126509.png" alt="" />
+                  <input type="tel" className=" editedImput " placeholder='TELEFONO ' /></li>
 
                 </div>
               <div>
-                  <li className="loginItem">
+                  <li className="loginItem"><img className='icon' src="https://cdn-icons-png.flaticon.com/512/3240/3240345.png" alt="" />
                   <input type="text" className=" editedImput " placeholder='ESPECIALIDAD' /></li>
 
-                  <li className="loginItem">
+                  <li className="loginItem"><img className='icon' src="https://cdn-icons-png.flaticon.com/512/3051/3051188.png" alt="" />
                   <input type="text" className=" editedImput " placeholder='INSTITUCION' /></li>
 
-                  <li className="loginItem">
+                  <li className="loginItem"><img className='icon' src="https://cdn-icons-png.flaticon.com/512/848/848043.png" alt="" />
                   <input type="text" className=" editedImput " placeholder='PAIS' /></li>
 
-                  <li className="loginItem">
+                  <li className="loginItem"><img className='icon' src="https://cdn-icons-png.flaticon.com/512/4830/4830731.png" alt="" />
                   <input type="text" className=" editedImput " placeholder='ESTADO' /></li>
 
-                  <li className="loginItem"><label></label>
-                  <input type="file" className="" placeholder='' /></li>
+                
+                  <li className="loginItem INfile" htmlFor="archivo">
+                  <label htmlFor="archivo">SUBIR SEDULA PROFECIONAL</label>
+                  <input type="file"className='File' id='archivo'/></li>
 
 
               </div>
@@ -56,13 +59,21 @@ function App() {
               </ul>
               <div>
               </div>
-              <button> REGISTRAME </button>
+              <button className='buttoIMf'> REGISTRAME </button>
             </form>
           </div>
           {/* <img src="https://cdn-icons-png.flaticon.com/512/7817/7817034.png" className='icono' /> */}
         </div>
+
+
+
+    <span className='circulo uno'></span>
+    <div className='circulo dos'></div>
+    <span className='circulo tres'></span>
+    <div className='circulo cuatro'></div>
+
       </div>
-    </div>
+    
   )
 }
 
