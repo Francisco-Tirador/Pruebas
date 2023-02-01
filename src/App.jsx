31c784373df9edc3c0062cc4ce307385a9788c42
Francisco Beltran <roshlandia@gmail.com>
2023-02-01 01:01:12 -0600

    dasd

--- src/App.jsx
+++ src/App.jsx
@@ -13,19 +13,19 @@ function App() {
 
 
 
-  var label1 = document.getElementById("label1");
-  var input1 = document.getElementById("input1");
+  // var label1 = document.getElementById("label1");
+  // var input1 = document.getElementById("input1");
 
-  input1.addEventListene("focus", e => {
-    if (input1.value === "") {
-      label1.classList.add("inputSeleccionado");
-    }
-  });
-  input1.addEventListener("blur", e => {
-    if (input1.value === "") {
-      label1.classList.remove("inputSeleccionado");
-    }
-  });
+  // input1.addEventListene("focus", e => {
+  //   if (input1.value === "") {
+  //     label1.classList.add("inputSeleccionado");
+  //   }
+  // });
+  // input1.addEventListener("blur", e => {
+  //   if (input1.value === "") {
+  //     label1.classList.remove("inputSeleccionado");
+  //   }
+  // });
 
 
   const change = () => {
@@ -47,31 +47,30 @@ function App() {
       <div className='image3'>
         <img src="https://socmexcirped.org/img2023/TEMPORAL2023.png" alt="" />
       </div>
-      <div class="grupoLI" id="xD">
-        <label for="" id="label1">First Name</label>
-        <input type="text" id="input1"/>
-      </div>
+     
       <div className='Contenedor3'>
 
         <div className="contenedor_campos_input">
           <p className='PALABRA'>SIGNIN</p>
           <div className='Bienvenida'>
-            <p>Bienvenido</p>
+            <p className='texto1'>Bienvenido</p>
+            <hr  className='hr'/>
             <p>Inicie sesión.</p>
           </div>
           <form action="">
             <ul>
               <li>
-                <label className='pal'>E-mail</label>
-                <input type="text" required className='inputM3' />
+                {/* <label className='pal'>E-mail</label> */}
+                <input   type="email" required className='inputM3' placeholder='E-mail'  />
 
               </li>
 
               <li>
-                <label className='pal'>Password</label>
-                <input type="email" className='inputM3' />
+                {/* <label className='pal'>Password</label> */}
+                <input type="password" className='inputM3' placeholder='Password'/>
               </li>
             </ul>
+            <button className='pald'>Iniciar sesión</button>
           </form>
         </div>
       </div>
